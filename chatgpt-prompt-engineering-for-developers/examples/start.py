b254e0f8e8ebdcbf585d60a7a9e921079ef08865
SparkLee <liweijsj@163.com>
2023-05-14 20:02:50 +0800

    update

--- chatgpt-prompt-engineering-for-developers/examples/start.py
+++ chatgpt-prompt-engineering-for-developers/examples/start.py
@@ -1,14 +1,4 @@
-#!/bin/python
-# -*- coding: utf-8 -*-
-
 import openai
-import os
-from dotenv import load_dotenv, find_dotenv
-
-_ = load_dotenv(find_dotenv())
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     '''
