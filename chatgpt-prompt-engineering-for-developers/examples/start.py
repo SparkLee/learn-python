f6a283eefe292c1a85a9a63d82b56516e18327ee
SparkLee <liweijsj@163.com>
2023-05-14 20:25:10 +0800

    update

--- chatgpt-prompt-engineering-for-developers/examples/start.py
+++ chatgpt-prompt-engineering-for-developers/examples/start.py
@@ -1,25 +1,4 @@
-import openai
-import os
-from dotenv import load_dotenv, find_dotenv
-
-_ = load_dotenv(find_dotenv())
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    '''
-    prompt: 对应的提示
-    model: 调用的模型，默认为 gpt-3.5-turbo(ChatGPT)，有内测资格的用户可以选择 gpt-4
-    '''
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,
-    )
-    return response.choices[0].message["content"]
-
+from bootstrap import get_completion
 
 text = f"""
 You should express what you want a model to do by \ 
@@ -40,5 +19,4 @@ into a single sentence.
 ```{text}```
 """
 
-response = get_completion(prompt)
-print(response)
+print(get_completion(prompt))
